e177c7e90ba2d463cc9328743bd6aa5abd70e7d1
“Vishnu <vvichugit@gmail.com>
2018-09-09 23:55:59 -0500

    this is the inital commit

--- src/router.js
+++ src/router.js
@@ -1,23 +1,31 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import Home from './views/Home.vue'
+import Vue from "vue";
+import Router from "vue-router";
+import Home from "./views/Home.vue";
+import pageb from "./views/pageb.vue";
 
-Vue.use(Router)
+Vue.use(Router);
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'home',
+      path: "/",
+      name: "home",
       component: Home
     },
     {
-      path: '/about',
-      name: 'about',
+      path: "/pageb",
+      name: "pageb",
+      component: pageb
+    },
+
+    {
+      path: "/about",
+      name: "about",
       // route level code-splitting
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+      component: () =>
+        import(/* webpackChunkName: "about" */ "./views/About.vue")
     }
   ]
-})
+});
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,69 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    
+
+   <nav class="navbar navbar-light" style="background-color: red;">
+      
+    <h2>Holy Shit!!!</h2>
+
+
+      <h2 >DG</h2>
+
+   </nav>
+
+
+
+    <router-link = "./pageb">
+   <button id="shy" v-on:click="ale()">Goes to Page b</button>
+ </router-link>
+
+
+
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+<style>
 
+   #shy{
+
+
+       position: absolute;
+       left:660px;
+       top:400px;
+
+   }
+
+
+
+</style>
+
+<script>
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  }
-}
+  data: function() {
+    return {
+      
+
+   
+
+
+
+
+    };
+  },
+  created: function() {},
+  methods: {
+
+    ale: function() {
+
+       
+
+    }
+
+    
+
+
+
+  },
+  computed: {}
+};
 </script>
